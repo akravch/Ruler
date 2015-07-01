b2cb553b7746c865c9fdfb17354509e63a7c26b8
Alexandr <akravch92@gmail.com>
2015-07-01 22:52:39 +0300

    Add "Stay On Top" menu item. Add non-implemented IsVertical property. Use simple DrawingBrush for background. Add drag possibility.

--- Ruler/RulerWindow.xaml.cs
+++ Ruler/RulerWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Ruler
@@ -10,6 +11,15 @@ namespace Ruler
     {
         public static readonly RoutedCommand CloseCommand = new RoutedUICommand("_Close", "Close", typeof(RulerWindow));
 
+        public static readonly DependencyProperty IsVerticalProperty = DependencyProperty.Register(
+            "IsVertical", typeof (bool), typeof (RulerWindow), new FrameworkPropertyMetadata(false, OnIsVerticalChanged));
+        
+        public bool IsVertical
+        {
+            get { return (bool) GetValue(IsVerticalProperty); }
+            set { SetValue(IsVerticalProperty, value); }
+        }
+
         public RulerWindow()
         {
             InitializeComponent();
@@ -18,6 +28,21 @@ namespace Ruler
         private void OnCloseExecuted(object sender, ExecutedRoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private static void OnIsVerticalChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }

2ea1d0ef603c706bee2313ee9a0469f099def1be
Alexandr <akravch92@gmail.com>
2015-07-01 21:15:14 +0300

    Add transparent window style. Add context menu close item.

---
+++ Ruler/RulerWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Ruler
+{
+    /// <summary>
+    /// Interaction logic for RulerWindow.xaml
+    /// </summary>
+    public partial class RulerWindow : Window
+    {
+        public static readonly RoutedCommand CloseCommand = new RoutedUICommand("_Close", "Close", typeof(RulerWindow));
+
+        public RulerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCloseExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
